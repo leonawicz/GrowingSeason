2d0a6d31aadeb6dd4825dc0c71471cff9b66bd9a
Matt Leonawicz <matt.rstat@gmail.com>
2016-03-03 11:02:18 -0700

    Update plot code

--- code/gs_gbm_preds_eval_plots.R
+++ code/gs_gbm_preds_eval_plots.R
@@ -3,7 +3,7 @@ pkgs <- list("rasterVis", "maptools", "ggplot2", "data.table", "dplyr", "tidyr")
 lapply(pkgs, function(x) library(x, character.only=T))
 
 load("data.RData") # d, d.stats, d.stats2, d.hm, sos, ecomask, yrs, cbpal
-load("gbm_preds_eval.RData") # cv.out, d.out, s1, sMean, sMean2002
+load("gbm_preds_eval.RData") # ri.out, cv.out, d.out, s1, sMean, sMean2002
 shpDir <- "C:/github/DataExtraction/data/shapefiles"
 eco_shp <- shapefile(file.path(shpDir, "AK_ecoregions/akecoregions.shp")) %>% spTransform(CRS(projection(sos))) %>%
   unionSpatialPolygons(eco_shp@data$LEVEL_2)
@@ -17,6 +17,14 @@ ggplot(cv.out, aes(x=Region, y=CV)) + geom_boxplot(fill="gray") + geom_point(pos
   theme_bw() + theme(legend.position="bottom") + labs(title="5-fold CV optimal trees distributions", y="Number of trees")
 dev.off()
 
+# Relative influence by year and region
+png(file.path(plotDir, paste0("gbm_RI_byRegion.png")), width=1600, height=1600, res=200)
+ggplot(ri.out %>% group_by(Region, Year, Predictor) %>% summarise(RI=mean(RI)), aes(factor(Year), RI, fill=Predictor)) + geom_bar(stat="identity", position="stack") +
+  scale_fill_manual(values=cbpal[-2]) + theme_bw(base_size=10) + theme(legend.position="bottom") + coord_flip() +
+  ggtitle(paste("Predictor relative influence on start of growing season")) +
+  facet_wrap(~Region, ncol=5)
+dev.off()
+
 # gbm observed vs. fitted values
 png(file.path(plotDir, paste0("gbm_ObsVsFitted_byRegion.png")), width=2000, height=2000, res=200)
 ggplot(filter(d.out, is.na(Run)) %>% dcast(Region + Year + Run ~ Source, value.var="SOS"), aes(x=Predicted, y=Observed, colour=Region)) +
@@ -26,7 +34,7 @@ ggplot(filter(d.out, is.na(Run)) %>% dcast(Region + Year + Run ~ Source, value.v
 dev.off()
 
 # historical predictions over observations time series
-clrs <- c("peru", "royalblue")
+clrs <- c("peru", "royalblue", "black", "red")
 png(file.path(plotDir, paste0("gbm_TSpreds_byRegion.png")), width=3200, height=1600, res=200)
 ggplot(filter(d.out, !is.na(Run) & Source!="Bias corrected"), aes(x=Year, y=SOS, colour=Source, group=interaction(Source, Run))) +
   scale_color_manual(values=clrs) + 
@@ -37,17 +45,23 @@ ggplot(filter(d.out, !is.na(Run) & Source!="Bias corrected"), aes(x=Year, y=SOS,
   geom_line(data=filter(d.out, Source=="Predicted" & is.na(Run)), colour=clrs[2], size=1) +
   geom_line(data=filter(d.out, Source=="Observed" & is.na(Run)), colour=clrs[1], size=1) + 
   geom_line(data=filter(d.out, Source=="Predicted" & is.na(Run)), colour=clrs[2], size=1, linetype=2) +
+  geom_point(data=d.preds %>% select(-Predicted) %>% distinct, aes(y=SOS, colour=NULL, group=NULL), colour="black", size=3) +
+  geom_point(data=d.preds, aes(y=Predicted, colour=NULL, group=NULL), colour="red", size=1, position=position_jitter(width=0.2, height=0)) +
   theme_bw() + theme(legend.position="bottom") + ggtitle("Observed and modeled start of growing season") +
   scale_x_continuous(breaks=c(1982,1990,2000,2010)) +
   guides(fill=guide_legend(override.aes=list(alpha=1)), colour=guide_legend(override.aes=list(alpha=1))) +
   facet_wrap(~Region, ncol=3, scales="free")
 dev.off()
 
+d.sd <- filter(d.out, Source=="Observed" | (!is.na(Run) & Source!="Bias corrected")) %>% group_by(Region, Source) %>% summarise(SD=sd(SOS))
+d.hoy.sd <- d.preds %>% group_by(Region) %>% summarise(`Global observered`=sd(unique(SOS)), `Predicted HOY`=sd(Predicted)) %>% melt(id.vars="Region", variable.name="Source", value.name="SD")
+d.sd <- bind_rows(d.sd, d.hoy.sd)
+
 # predictions and observations historical inter-annual variability
 png(file.path(plotDir, paste0("gbm_TSpreds_byRegionSD.png")), width=3200, height=1600, res=200)
-ggplot(filter(d.out, Source=="Observed" | (!is.na(Run) & Source!="Bias corrected")) %>% group_by(Region, Source) %>% summarise(SD=sd(SOS)), aes(x=Region, y=SD, fill=Source)) +
+ggplot(d.sd, aes(x=Region, y=SD, fill=Source)) +
   scale_fill_manual(values=clrs) + geom_bar(stat="identity", position="dodge") +
-  theme_bw() + theme(legend.position="bottom") + ggtitle("Observed and modeled start of growing season period SD")
+  theme_bw() + theme(legend.position="bottom") + ggtitle("Observed and modeled start of growing season historical inter-annual SD")
 dev.off()
 
 # Maps
